1a4c7fc9eac1249ba19bf89ff3adcb569b9fda98
peach <pg@clicsoft.ch>
2016-05-13 17:45:32 +0200

    DB-Migration; Zwei unterschiedliche Queries zur Lösung der Aufgabe; Vollständiger Dump der "profs" Collection als JSON-Array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Uni.Hören;
 import Uni.Professoren;
+import Uni.Studenten;
 import Uni.Vorlesungen;
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 
 import javax.persistence.EntityManager;
@@ -24,14 +27,36 @@ public class Main {
         EntityManager em = sqlConnector.getEm();
 
         Query namedQuery = em.createNamedQuery("Professoren.findAll");
-        Collection<Professoren> professoren = (Collection<Professoren>)namedQuery.getResultList();
+        Collection<Professoren> professoren = (Collection<Professoren>) namedQuery.getResultList();
 
         for (Professoren p : professoren) {
-            BasicDBObject doc = new BasicDBObject("id", p.getPersNr())
+            BasicDBObject professor = new BasicDBObject("_id", p.getPersNr())
                     .append("name", p.getName())
                     .append("rang", p.getRang())
                     .append("raum", p.getRaum());
-            mongoDbConnector.getCollection().insert(doc);
+
+
+            BasicDBList vorlesungen = new BasicDBList();
+            for (Vorlesungen v : p.getVorlesungensByPersNr()) {
+                BasicDBObject vorlesung = new BasicDBObject("_id", v.getVorlNr())
+                        .append("titel", v.getTitel())
+                        .append("sws", v.getSws());
+
+                BasicDBList studenten = new BasicDBList();
+                for (Hören h : v.getHörensByVorlNr()) {
+                    Studenten s = h.getStudentenByMatrNr();
+                    BasicDBObject student = new BasicDBObject("_id", s.getMatrNr())
+                            .append("name", s.getName())
+                            .append("semester", s.getSemester());
+                    studenten.add(student);
+                }
+                vorlesung.append("studenten", studenten);
+                vorlesungen.add(vorlesung);
+            }
+
+            professor.append("vorlesungen", vorlesungen);
+
+            mongoDbConnector.getCollection().insert(professor);
             System.out.println("Insertet " + p.getName());
         }
     }
